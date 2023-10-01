9e5476460341d5ec7b882b10ef197b684f315c0b
JeromeSchreiberTR <jerome.schreiber@thomsonreuters.com>
2023-10-01 18:24:26 -0500

    Home Page Articles and Logo updates

--- resources/js/index.js
+++ resources/js/index.js
@@ -10,3 +10,39 @@ document.querySelector('a[href="#OurServices"]').addEventListener('click', funct
       });
     }
   });
+
+
+  document.addEventListener('DOMContentLoaded', function () {
+    fetch('resources/articles.json')
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Network Response Error');
+            }
+            return response.json();
+        })
+        .then(articles => {
+            const container = document.querySelector('.homeBlogPostsContainer');
+
+            articles.forEach(data => {
+                if(data.isHighlighted == "yes"){
+                  const articleLink = document.createElement('a');
+                  articleLink.classList.add('articleLink');
+                  articleLink.href = "article.html?" + data.link;
+                  
+                  const articleContainer = document.createElement('div');
+                  articleContainer.classList.add('articleContainer');
+                  articleContainer.innerHTML = `
+                      <h6 class="articleDate">${data.date}</h6>
+                      <h1 class="articleTitle">${data.title}</h1>
+                      <h5 class="articleAuthor">${data.author}</h5>
+                      <div class="articleSummary">${data.summary}</div>
+                  `;
+                  articleLink.appendChild(articleContainer)
+                  container.appendChild(articleLink);
+              }
+            });
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        })
+});
